aac1f174580a4f1fb8b4bb042b0199624c81c80c
on-ur-mark <113871352+on-ur-mark@users.noreply.github.com>
2022-10-27 01:43:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+AOS.init();
 
+
+/* topBtn 클릭시 제일 위로 */
+$(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop()>3400){
+      $('#topBtn').fadeIn();
+    }else{
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $('#topBtn').click(function(){
+    $('html, body').animate({
+      scrollTop : 0
+    }, 400);
+  });
+});

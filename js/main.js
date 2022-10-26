b8f27ef2f59ed207edfeb3dd2c46076a53011973
on-ur-mark <113871352+on-ur-mark@users.noreply.github.com>
2022-10-27 01:58:07 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ AOS.init();
 /* topBtn 클릭시 제일 위로 */
 $(function(){
   $(window).scroll(function(){
-    if ($(this).scrollTop()>3000){
+    if ($(this).scrollTop()>500){
       $('#topBtn').fadeIn();
     }else{
       $('#topBtn').fadeOut();
